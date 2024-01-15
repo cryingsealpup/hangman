dad7447db71716b6c403555ef1ef891a02803477
Ekaterina Radchenko <ekatherinered@gmail.com>
2024-01-15 20:49:25 +0400

    feat: finished layout, creation from js

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const keyboard = document.createElement('div'), question = document.createElemen
       imgGallow = document.createElement('img'), hangman = document.createElement('div'), head = document.createElement('img'),
       hands = document.createElement('div'), leftHand = document.createElement('img'), rightHand = document.createElement('img'),
       corpus = document.createElement('img'), legs = document.createElement('div'), leftLeg = document.createElement('img'), 
-      rightLeg = document.createElement('img'), wrapper = document.createElement('div'), body = document.querySelector('body'),
-      answer = document.createElement('div'), keys = Array.from({ length: 122 - 91 }, (_, i) => String.fromCharCode(i + 97))
+      rightLeg = document.createElement('img'), wrapper = document.createElement('div'), leftSide = document.createElement('div'),
+      h1 = document.createElement('h1'), body = document.querySelector('body'), answer = document.createElement('div'), 
+      guesses = document.createElement('div'), keys = Array.from({ length: 117 - 91 }, (_, i) => String.fromCharCode(i + 97))
 
 let correct = [], wrong = [], counter = 0, word = ''
 
+keys.push(String.fromCharCode(30 + 97))
 
 keys.forEach(key => {
     const button = document.createElement("button")
@@ -37,10 +39,83 @@ setAttributes([
         attr: ['src', 'alt'], 
         val: ['./images/gallows.svg', 'Gallow' ]
     },
+    {
+        el: hangman, 
+        attr: 'class', 
+        val: 'hangman'
+    },
+    {
+        el: head, 
+        attr: ['src', 'alt','class'], 
+        val: ["./images/head.svg", 'Hangman head', 'hangman-head']
+    },
+    {
+        el: hands, 
+        attr: 'class', 
+        val: 'hangman-hands '
+    },
+    {
+        el: leftHand, 
+        attr: ['src', 'alt'], 
+        val: ["./images/hand-one.svg", 'Left hand']
+    },
+    {
+        el: rightHand, 
+        attr: ['src', 'alt'], 
+        val: ["./images/hand-two.svg", 'Right hand']
+    },
+    {
+        el: corpus, 
+        attr: ['src', 'alt', 'class'], 
+        val: ["./images/body.svg", 'Hangman body', 'hangman-body']
+    },
+    {
+        el: legs, 
+        attr: 'class', 
+        val: 'hangman-legs'
+    },
+    {
+        el: leftLeg, 
+        attr: ['src', 'alt'], 
+        val: ["./images/leg-one.svg", 'Left leg']
+    },
+    {
+        el: rightLeg, 
+        attr: ['src', 'alt'], 
+        val: ["./images/leg-two.svg", 'Right leg']
+    },
+    {
+        el: wrapper, 
+        attr: 'class', 
+        val: 'game'
+    },
+    {
+        el: leftSide, 
+        attr: 'class', 
+        val: 'logic'
+    },
+    {
+        el: answer, 
+        attr: 'class', 
+        val: 'answer'
+    },
+    {
+        el: guesses, 
+        attr: 'class', 
+        val: 'guesses'
+    },
 ])
 getRandomWord()
-body.appendChild(question)
-body.appendChild(keyboard)
+
+h1.textContent = 'Hangman Game'
+
+hands.append(leftHand, rightHand)
+legs.append(leftLeg, rightLeg)
+hangman.append(head, hands, corpus, legs)
+gallow.append(imgGallow, hangman)
+leftSide.append(question, answer, guesses, keyboard)
+wrapper.append(gallow, leftSide)
+body.append(h1, wrapper)
 
 keyboard.addEventListener('click', e => {
     console.log(e.target)
@@ -51,22 +126,24 @@ keyboard.addEventListener('click', e => {
 
 function setAttributes(elements) {
     elements.forEach((item) => {
-        typeof item.attr === 'string' ? item.element.setAttribute(item.attr, item.val) : item.attr.forEach((attr, i) => {
-            item.element.setAttribute(attr, item.val[i])
+        typeof item.attr === 'string' ? item.el.setAttribute(item.attr, item.val) : item.attr.forEach((attr, i) => {
+            item.el.setAttribute(attr, item.val[i])
         })
     })
 }
 
-const getRandomWord = () => {
+function getRandomWord() {
     const { q, a } = pairs[Math.floor(Math.random() * pairs.length)]
     word = a
     question.textContent = q
+    answer.textContent = "_ ".repeat(word.length)
     resetGame()
 }
 
-const resetGame = () => {
+function resetGame() {
     correct = [];
     counter = 0;
+    guesses.textContent = 0
    // guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
   //  wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
   //  keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
@@ -79,3 +156,15 @@ const resetGame = () => {
 //     keyboard.appendChild(button);
 //  //   button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
 // }
+
+
+// LISTENERS
+document.addEventListener("keydown", (event) => {
+    if (keys.includes(event.key.toLowerCase())) {
+      //console.log(keys.find(event.key.toLowerCase()))
+            console.log(event.key.toLowerCase())
+    } else if (event.key === ' ') {
+
+    }
+    // do something
+});
